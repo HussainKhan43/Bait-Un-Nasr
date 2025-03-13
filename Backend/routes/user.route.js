33f3c78e5c692b31f05a1ad5f4e8f29a1ae2e55f
HussainKhan43 <hussainkhan5march@gmail.com>
2025-03-13 23:00:36 +0530

    Updated Frontend files

--- Backend/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { contact } from "../controller/user.controller.js"; 
-
-const router = express.Router();
-
-router.post("/contact", contact);
-
-export default router;
